b0588cbc83667cbc112619719a9453d16301b337
Mario756 <mariomazziotti756@gmail.com>
2023-10-22 17:42:21 +0200

    Logic of MainActivity
    Added the ui of the main activity:
    -Created the liveBmi for showing the result in real time
    -Created a viewModel where there are some functions:
        \\ Setter Height and Weight where all the data are setted
        \\ CalcBmi that calculates the bmi of the person
        \\ ShowToast that shows a toast if some fields are empty

--- app/src/main/java/com/example/bodymassindex/MainActivity.kt
+++ app/src/main/java/com/example/bodymassindex/MainActivity.kt
@@ -1,11 +1,98 @@
 package com.example.bodymassindex
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.bodymassindex.databinding.ActivityMainBinding
+import com.example.bodymassindex.viewModel.BmiViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val bmiViewModel = BmiViewModel()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = bmiViewModel
+
+        val weight = binding.Weight.text.toString()
+        val height = binding.Height.text.toString()
+        val radioGroup = binding.Sex
+        val elements = binding.Sex.childCount
+
+
+
+        binding.Weight.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                bmiViewModel.setWeight(weight.toDouble())
+                bmiViewModel.calcBmi()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.Height.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                bmiViewModel.setHeight(height.toDouble())
+                bmiViewModel.calcBmi()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+
+
+        binding.CalculateButton.setOnClickListener{
+
+
+            var isActive = false
+            for (i in 0 until elements) {
+                val radioButton = radioGroup.getChildAt(i) as RadioButton
+
+                if (radioButton.isChecked) {
+                    isActive = true
+                    break
+                }
+            }
+
+            if(weight.isEmpty() && weight.length <= 2){
+                bmiViewModel.showToast(this , getString(R.string.ErrorWeight) , 30)
+            } else if (height.isEmpty() && height.length <= 3){
+                bmiViewModel.showToast(this , getString(R.string.ErrorHeight) , 30)
+            } else if(!isActive){
+                bmiViewModel.showToast(this , getString(R.string.ErrorSex) , 30)
+            } else {
+                bmiViewModel.calcBmi()
+            }
+
+        }
+
+
     }
+
+//    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+//        return super.onCreateView(name, context, attrs)
+//    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/bodymassindex/viewModel/BmiViewModel.kt
+++ app/src/main/java/com/example/bodymassindex/viewModel/BmiViewModel.kt
@@ -1,5 +1,50 @@
 package com.example.bodymassindex.viewModel
 
-class BmiViewModel {
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BmiViewModel : ViewModel() {
+
+    private val _liveBmi = MutableLiveData<Double>(00.0)
+    val liveBmi: LiveData<Double>
+        get() = _liveBmi
+
+    //Setter and Getter
+
+    private var _weight = MutableLiveData(0.00)
+    var weight: LiveData<Double> = _weight
+    fun setWeight(weight: Double) {
+        if (weight != _weight.value) {
+            _weight.value = weight
+        }
+    }
+
+    private var _height = MutableLiveData(0.00)
+    var height: LiveData<Double> = _height
+    fun setHeight(height: Double) {
+        if (height != _height.value) {
+            _height.value = height
+        }
+    }
+
+    fun showToast(context: Context, msg: String, duration: Int) {
+        Toast.makeText(context, msg, duration).show()
+    }
+
+
+    fun calcBmi() {
+        val convertedCmToMetres = height.value?.div(100.0f)
+
+        val Bmi = weight.value!! / (convertedCmToMetres!! * convertedCmToMetres)
+        _liveBmi.value = Bmi
+    }
+
 
 }
+
+
+
+
